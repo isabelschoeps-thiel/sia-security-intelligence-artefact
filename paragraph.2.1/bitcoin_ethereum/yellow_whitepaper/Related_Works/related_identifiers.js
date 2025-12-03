1d50477e5c88f491d8997760eb0607d807926891
Isabel Schöps Thiel <u5983574840@gmail.com>
2025-12-03 22:34:38 +0100

    Update related_identifiers.js
    Signed-off-by: Isabel Schöps Thiel <u5983574840@gmail.com>

--- paragraph.2.1/bitcoin_ethereum/yellow_whitepaper/Related_Works/related_identifiers.js
+++ paragraph.2.1/bitcoin_ethereum/yellow_whitepaper/Related_Works/related_identifiers.js
@@ -1,3 +1,8 @@
 Is derived from → DOI: 10.1016/j.future.2019.12.019
 Relation type: Derived from
 Resource type: Journal article
+Is derived from → DOI: 10.1093/bioinformatics/bth078 (OUP, 2004)
+Is derived from → DOI: 10.1016/j.future.2019.12.019 (Elsevier, 2020)
+Is supplemented by → referenzen-buecher-37.000-5000-journal.pdf
+Is documented by → isabelschoepsthiel-oxford-copyright.pdf
+Is documented by → Elvisr-isabelschoepsthiel.pdf

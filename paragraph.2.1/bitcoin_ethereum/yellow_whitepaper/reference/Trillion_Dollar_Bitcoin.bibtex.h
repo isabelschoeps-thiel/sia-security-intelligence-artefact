ccae1ac998c2df08e816ed3858c97a27bcef4316
Isabel Schöps Thiel <u5983574840@gmail.com>
2025-12-03 22:39:50 +0100

    reference
    copyright 
    
    Signed-off-by: Isabel Schöps Thiel <u5983574840@gmail.com>

---
+++ paragraph.2.1/bitcoin_ethereum/yellow_whitepaper/reference/Trillion_Dollar_Bitcoin.bibtex.h
@@ -0,0 +1,8 @@
+@book{nakamoto2020trillion,
+  title={Trillion Dollar Bitcoin},
+  author={Nakamoto, S.},
+  isbn={9798642790113},
+  url={https://books.google.de/books?id=oU2bzQEACAAJ},
+  year={2020},
+  publisher={Independently Published}
+}
---
+++ paragraph.2.1/bitcoin_ethereum/yellow_whitepaper/reference/Trillion_Dollar_Bitcoin.ris.h
@@ -0,0 +1,8 @@
+TY  - BOOK
+T1  - Trillion Dollar Bitcoin
+A1  - Nakamoto, S.
+SN  - 9798642790113
+UR  - https://books.google.de/books?id=oU2bzQEACAAJ
+Y1  - 2020
+PB  - Independently Published
+ER  - 

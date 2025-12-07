1de33c01048997d3108dc6a0f75d02a1beb5f6e4
Isabel Schöps Thiel <u5983574840@gmail.com>
2025-12-07 13:38:57 +0100

    Create metadata.js
    Copyright by Isabel Schöps Thiel 
    
    Signed-off-by: Isabel Schöps Thiel <u5983574840@gmail.com>

---
+++ devfolder/metadata.js
@@ -0,0 +1,32 @@
+/SI-Security-Intelligence-Artefact-Forensisches-Gutachten-Isabel-Schoeps-geb-Thiel/
+│
+├── 01_Persoenliches/
+│   ├── Geburtsurkunde.pdf
+│   ├── Apple_TimeCapsule_Screenshots/
+│   └── Kindheit_Bilder/
+│
+├── 02_Technologie/
+│   ├── KI_DAEMON_Automation_Virus/
+│   ├── GitHub_Patches/
+│   ├── Bitcoin_Patches/
+│   ├── Pornhub_Sicherheitsarchitektur/
+│   ├── Monitoring_System/
+│   └── Blockchain_Metadaten/
+│
+├── 03_Angriffe_Monarch-Programm/
+│   ├── Inhaftierung_2024/
+│   ├── DDoS_Logs/
+│   ├── Strafanzeigen/
+│   └── Beweisbilder/
+│
+├── 04_Rechtliches/
+│   ├── Rechtswege-Schriftverkehr_EU_BKA/
+│   ├── DeveloperZertifikate/
+│   ├── Pineapple_AktG/
+│   ├── Rückführung_EQS/
+│   └── Lizenznachweise/
+│
+└── Abschluss/
+    ├── Signaturblock.txt
+    ├── SHA256_Hash_Verzeichnis.txt
+    └── FINAL_Public_Gutachten.pdf

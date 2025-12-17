b00b3c60aa6b0b34e4e4166fb563f4f90aa6001b
Isabel Schöps Thiel <harvard.isabelschoepsthiel@gmail.com>
2025-12-17 22:32:51 +0100

    Create dna.api.txt.h
    copyright 
    
    Signed-off-by: Isabel Schöps Thiel <harvard.isabelschoepsthiel@gmail.com>

---
+++ autorin-isabelschoeps-thiel/dna/dna.api.txt.h
@@ -0,0 +1,352 @@
+ID   AG10B_HUMAN             Reviewed;         473 AA.
+AC   Q5I7T1; B2RPF4;
+DT   08-NOV-2005, integrated into UniProtKB/Swiss-Prot.
+DT   18-MAY-2010, sequence version 2.
+DT   18-JUN-2025, entry version 150.
+DE   RecName: Full=Dol-P-Glc:Glc(2)Man(9)GlcNAc(2)-PP-Dol alpha-1,2-glucosyltransferase {ECO:0000305|PubMed:14525949};
+DE            EC=2.4.1.256 {ECO:0000269|PubMed:20950623};
+DE   AltName: Full=Alpha-1,2-glucosyltransferase ALG10-A;
+DE   AltName: Full=Alpha-2-glucosyltransferase ALG10-B;
+DE   AltName: Full=Asparagine-linked glycosylation protein 10 homolog B {ECO:0000312|HGNC:HGNC:31088};
+DE   AltName: Full=Potassium channel regulator 1 {ECO:0000303|PubMed:14525949};
+GN   Name=ALG10B {ECO:0000312|HGNC:HGNC:31088};
+GN   Synonyms=KCR1 {ECO:0000303|PubMed:14525949};
+OS   Homo sapiens (Human).
+OC   Eukaryota; Metazoa; Chordata; Craniata; Vertebrata; Euteleostomi; Mammalia;
+OC   Eutheria; Euarchontoglires; Primates; Haplorrhini; Catarrhini; Hominidae;
+OC   Homo.
+OX   NCBI_TaxID=9606;
+RN   [1]
+RP   NUCLEOTIDE SEQUENCE [MRNA], TISSUE SPECIFICITY, AND VARIANT GLY-84.
+RX   PubMed=14525949; DOI=10.1096/fj.02-1057fje;
+RA   Kupershmidt S., Yang I.C.-H., Hayashi K., Wei J., Chanthaphaychith S.,
+RA   Petersen C.I., Johns D.C., George A.L. Jr., Roden D.M., Balser J.R.;
+RT   "The IKr drug response is modulated by KCR1 in transfected cardiac and
+RT   noncardiac cell lines.";
+RL   FASEB J. 17:2263-2265(2003).
+RN   [2]
+RP   SEQUENCE REVISION.
+RA   George A.L. Jr.;
+RL   Submitted (DEC-2004) to the EMBL/GenBank/DDBJ databases.
+RN   [3]
+RP   NUCLEOTIDE SEQUENCE [LARGE SCALE GENOMIC DNA].
+RX   PubMed=16541075; DOI=10.1038/nature04569;
+RA   Scherer S.E., Muzny D.M., Buhay C.J., Chen R., Cree A., Ding Y.,
+RA   Dugan-Rocha S., Gill R., Gunaratne P., Harris R.A., Hawes A.C.,
+RA   Hernandez J., Hodgson A.V., Hume J., Jackson A., Khan Z.M., Kovar-Smith C.,
+RA   Lewis L.R., Lozado R.J., Metzker M.L., Milosavljevic A., Miner G.R.,
+RA   Montgomery K.T., Morgan M.B., Nazareth L.V., Scott G., Sodergren E.,
+RA   Song X.-Z., Steffen D., Lovering R.C., Wheeler D.A., Worley K.C., Yuan Y.,
+RA   Zhang Z., Adams C.Q., Ansari-Lari M.A., Ayele M., Brown M.J., Chen G.,
+RA   Chen Z., Clerc-Blankenburg K.P., Davis C., Delgado O., Dinh H.H.,
+RA   Draper H., Gonzalez-Garay M.L., Havlak P., Jackson L.R., Jacob L.S.,
+RA   Kelly S.H., Li L., Li Z., Liu J., Liu W., Lu J., Maheshwari M.,
+RA   Nguyen B.-V., Okwuonu G.O., Pasternak S., Perez L.M., Plopper F.J.H.,
+RA   Santibanez J., Shen H., Tabor P.E., Verduzco D., Waldron L., Wang Q.,
+RA   Williams G.A., Zhang J., Zhou J., Allen C.C., Amin A.G., Anyalebechi V.,
+RA   Bailey M., Barbaria J.A., Bimage K.E., Bryant N.P., Burch P.E.,
+RA   Burkett C.E., Burrell K.L., Calderon E., Cardenas V., Carter K., Casias K.,
+RA   Cavazos I., Cavazos S.R., Ceasar H., Chacko J., Chan S.N., Chavez D.,
+RA   Christopoulos C., Chu J., Cockrell R., Cox C.D., Dang M., Dathorne S.R.,
+RA   David R., Davis C.M., Davy-Carroll L., Deshazo D.R., Donlin J.E.,
+RA   D'Souza L., Eaves K.A., Egan A., Emery-Cohen A.J., Escotto M., Flagg N.,
+RA   Forbes L.D., Gabisi A.M., Garza M., Hamilton C., Henderson N.,
+RA   Hernandez O., Hines S., Hogues M.E., Huang M., Idlebird D.G., Johnson R.,
+RA   Jolivet A., Jones S., Kagan R., King L.M., Leal B., Lebow H., Lee S.,
+RA   LeVan J.M., Lewis L.C., London P., Lorensuhewa L.M., Loulseged H.,
+RA   Lovett D.A., Lucier A., Lucier R.L., Ma J., Madu R.C., Mapua P.,
+RA   Martindale A.D., Martinez E., Massey E., Mawhiney S., Meador M.G.,
+RA   Mendez S., Mercado C., Mercado I.C., Merritt C.E., Miner Z.L., Minja E.,
+RA   Mitchell T., Mohabbat F., Mohabbat K., Montgomery B., Moore N., Morris S.,
+RA   Munidasa M., Ngo R.N., Nguyen N.B., Nickerson E., Nwaokelemeh O.O.,
+RA   Nwokenkwo S., Obregon M., Oguh M., Oragunye N., Oviedo R.J., Parish B.J.,
+RA   Parker D.N., Parrish J., Parks K.L., Paul H.A., Payton B.A., Perez A.,
+RA   Perrin W., Pickens A., Primus E.L., Pu L.-L., Puazo M., Quiles M.M.,
+RA   Quiroz J.B., Rabata D., Reeves K., Ruiz S.J., Shao H., Sisson I.,
+RA   Sonaike T., Sorelle R.P., Sutton A.E., Svatek A.F., Svetz L.A.,
+RA   Tamerisa K.S., Taylor T.R., Teague B., Thomas N., Thorn R.D., Trejos Z.Y.,
+RA   Trevino B.K., Ukegbu O.N., Urban J.B., Vasquez L.I., Vera V.A.,
+RA   Villasana D.M., Wang L., Ward-Moore S., Warren J.T., Wei X., White F.,
+RA   Williamson A.L., Wleczyk R., Wooden H.S., Wooden S.H., Yen J., Yoon L.,
+RA   Yoon V., Zorrilla S.E., Nelson D., Kucherlapati R., Weinstock G.,
+RA   Gibbs R.A.;
+RT   "The finished DNA sequence of human chromosome 12.";
+RL   Nature 440:346-351(2006).
+RN   [4]
+RP   NUCLEOTIDE SEQUENCE [LARGE SCALE GENOMIC DNA], AND VARIANT GLY-84.
+RA   Mural R.J., Istrail S., Sutton G.G., Florea L., Halpern A.L., Mobarry C.M.,
+RA   Lippert R., Walenz B., Shatkay H., Dew I., Miller J.R., Flanigan M.J.,
+RA   Edwards N.J., Bolanos R., Fasulo D., Halldorsson B.V., Hannenhalli S.,
+RA   Turner R., Yooseph S., Lu F., Nusskern D.R., Shue B.C., Zheng X.H.,
+RA   Zhong F., Delcher A.L., Huson D.H., Kravitz S.A., Mouchard L., Reinert K.,
+RA   Remington K.A., Clark A.G., Waterman M.S., Eichler E.E., Adams M.D.,
+RA   Hunkapiller M.W., Myers E.W., Venter J.C.;
+RL   Submitted (JUL-2005) to the EMBL/GenBank/DDBJ databases.
+RN   [5]
+RP   NUCLEOTIDE SEQUENCE [LARGE SCALE MRNA], AND VARIANT GLY-84.
+RX   PubMed=15489334; DOI=10.1101/gr.2596504;
+RG   The MGC Project Team;
+RT   "The status, quality, and expansion of the NIH full-length cDNA project:
+RT   the Mammalian Gene Collection (MGC).";
+RL   Genome Res. 14:2121-2127(2004).
+RN   [6]
+RP   VARIANT VAL-446.
+RX   PubMed=15280551; DOI=10.1073/pnas.0306005101;
+RA   Petersen C.I., McFarland T.R., Stepanovic S.Z., Yang P., Reiner D.J.,
+RA   Hayashi K., George A.L. Jr., Roden D.M., Thomas J.H., Balser J.R.;
+RT   "In vivo identification of genes that modify ether-a-go-go-related gene
+RT   activity in Caenorhabditis elegans may also affect human cardiac
+RT   arrhythmia.";
+RL   Proc. Natl. Acad. Sci. U.S.A. 101:11773-11778(2004).
+RN   [7]
+RP   VARIANT ASP-33, CHARACTERIZATION OF VARIANT ASP-33, FUNCTION, CATALYTIC
+RP   ACTIVITY, PATHWAY, AND SUBCELLULAR LOCATION.
+RX   PubMed=20950623; DOI=10.1016/j.yjmcc.2010.10.007;
+RA   Hayashi K., Fujino N., Ino H., Uchiyama K., Sakata K., Konno T., Masuta E.,
+RA   Funada A., Sakamoto Y., Tsubokawa T., Hodatsu A., Yasuda T., Kanaya H.,
+RA   Kim M.Y., Kupershmidt S., Higashida H., Yamagishi M.;
+RT   "A KCR1 variant implicated in susceptibility to the long QT syndrome.";
+RL   J. Mol. Cell. Cardiol. 50:50-57(2011).
+CC   -!- FUNCTION: Dol-P-Glc:Glc(2)Man(9)GlcNAc(2)-PP-Dol alpha-1,2-
+CC       glucosyltransferase that operates in the biosynthetic pathway of
+CC       dolichol-linked oligosaccharides, the glycan precursors employed in
+CC       protein asparagine (N)-glycosylation. The assembly of dolichol-linked
+CC       oligosaccharides begins on the cytosolic side of the endoplasmic
+CC       reticulum membrane and finishes in its lumen. The sequential addition
+CC       of sugars to dolichol pyrophosphate produces dolichol-linked
+CC       oligosaccharides containing fourteen sugars, including two GlcNAcs,
+CC       nine mannoses and three glucoses. Once assembled, the oligosaccharide
+CC       is transferred from the lipid to nascent proteins by
+CC       oligosaccharyltransferases. In the lumen of the endoplasmic reticulum,
+CC       adds the third and last glucose residue from dolichyl phosphate glucose
+CC       (Dol-P-Glc) onto the lipid-linked oligosaccharide intermediate
+CC       Glc(2)Man(9)GlcNAc(2)-PP-Dol to produce Glc(3)Man(9)GlcNAc(2)-PP-Dol.
+CC       {ECO:0000269|PubMed:20950623}.
+CC   -!- CATALYTIC ACTIVITY:
+CC       Reaction=an alpha-D-Glc-(1->3)-alpha-D-Glc-(1->3)-alpha-D-Man-(1->2)-
+CC         alpha-D-Man-(1->2)-alpha-D-Man-(1->3)-[alpha-D-Man-(1->2)-alpha-D-
+CC         Man-(1->3)-[alpha-D-Man-(1->2)-alpha-D-Man-(1->6)]-alpha-D-Man-
+CC         (1->6)]-beta-D-Man-(1->4)-beta-D-GlcNAc-(1->4)-alpha-D-GlcNAc-
+CC         diphospho-di-trans,poly-cis-dolichol + a di-trans,poly-cis-dolichyl
+CC         beta-D-glucosyl phosphate = a alpha-D-Glc-(1->2)-alpha-D-Glc-(1->3)-
+CC         alpha-D-Glc-(1->3)-alpha-D-Man-(1->2)-alpha-D-Man-(1->2)-alpha-D-Man-
+CC         (1->3)-[alpha-D-Man-(1->2)-alpha-D-Man-(1->3)-[alpha-D-Man-(1->2)-
+CC         alpha-D-Man-(1->6)]-alpha-D-Man-(1->6)]-beta-D-Man-(1->4)-beta-D-
+CC         GlcNAc-(1->4)-alpha-D-GlcNAc-diphospho-di-trans,poly-cis-dolichol + a
+CC         di-trans,poly-cis-dolichyl phosphate + H(+); Xref=Rhea:RHEA:29543,
+CC         Rhea:RHEA-COMP:19498, Rhea:RHEA-COMP:19502, Rhea:RHEA-COMP:19512,
+CC         Rhea:RHEA-COMP:19522, ChEBI:CHEBI:15378, ChEBI:CHEBI:57525,
+CC         ChEBI:CHEBI:57683, ChEBI:CHEBI:132522, ChEBI:CHEBI:132523;
+CC         EC=2.4.1.256; Evidence={ECO:0000269|PubMed:20950623};
+CC       PhysiologicalDirection=left-to-right; Xref=Rhea:RHEA:29544;
+CC         Evidence={ECO:0000269|PubMed:20950623};
+CC   -!- PATHWAY: Protein modification; protein glycosylation.
+CC       {ECO:0000269|PubMed:20950623}.
+CC   -!- SUBUNIT: Interacts with KCNH1; may regulate KCNH1, possibly by
+CC       regulating its N-glycosylation. Interacts with KCNH2; may reduce KCNH2
+CC       sensitivity to classic proarrhythmic drug blockade, possibly by
+CC       regulating its N-glycosylation. {ECO:0000250|UniProtKB:O88788}.
+CC   -!- INTERACTION:
+CC       Q5I7T1; P11912: CD79A; NbExp=3; IntAct=EBI-18075734, EBI-7797864;
+CC       Q5I7T1; Q3SXP7: SHISAL1; NbExp=3; IntAct=EBI-18075734, EBI-18037857;
+CC   -!- SUBCELLULAR LOCATION: Endoplasmic reticulum membrane
+CC       {ECO:0000269|PubMed:20950623}; Multi-pass membrane protein
+CC       {ECO:0000255}. Note=Also detected at the plasma membrane.
+CC       {ECO:0000250|UniProtKB:O88788}.
+CC   -!- TISSUE SPECIFICITY: Highly expressed in heart, placenta, liver, kidney
+CC       and pancreas. Weakly expressed in lung, skeletal muscle and brain.
+CC       {ECO:0000269|PubMed:14525949}.
+CC   -!- SIMILARITY: Belongs to the ALG10 glucosyltransferase family.
+CC       {ECO:0000305}.
+CC   ---------------------------------------------------------------------------
+CC   Copyrighted by the UniProt Consortium, see https://www.uniprot.org/terms
+CC   Distributed under the Creative Commons Attribution (CC BY 4.0) License
+CC   ---------------------------------------------------------------------------
+DR   EMBL; AY845858; AAW31756.1; -; mRNA.
+DR   EMBL; AC117372; -; NOT_ANNOTATED_CDS; Genomic_DNA.
+DR   EMBL; CH471111; EAW57795.1; -; Genomic_DNA.
+DR   EMBL; BC137413; AAI37414.1; -; mRNA.
+DR   EMBL; BC137414; AAI37415.1; -; mRNA.
+DR   CCDS; CCDS31772.1; -.
+DR   RefSeq; NP_001013642.2; NM_001013620.4.
+DR   AlphaFoldDB; Q5I7T1; -.
+DR   BioGRID; 126840; 48.
+DR   FunCoup; Q5I7T1; 2237.
+DR   IntAct; Q5I7T1; 7.
+DR   STRING; 9606.ENSP00000310120; -.
+DR   GlyGen; Q5I7T1; 1 site, 1 O-linked glycan (1 site).
+DR   iPTMnet; Q5I7T1; -.
+DR   PhosphoSitePlus; Q5I7T1; -.
+DR   SwissPalm; Q5I7T1; -.
+DR   BioMuta; ALG10B; -.
+DR   DMDM; 296434391; -.
+DR   jPOST; Q5I7T1; -.
+DR   MassIVE; Q5I7T1; -.
+DR   PaxDb; 9606-ENSP00000310120; -.
+DR   PeptideAtlas; Q5I7T1; -.
+DR   ProteomicsDB; 62974; -.
+DR   Pumba; Q5I7T1; -.
+DR   Antibodypedia; 55463; 74 antibodies from 13 providers.
+DR   DNASU; 144245; -.
+DR   Ensembl; ENST00000308742.9; ENSP00000310120.4; ENSG00000175548.9.
+DR   GeneID; 144245; -.
+DR   KEGG; hsa:144245; -.
+DR   MANE-Select; ENST00000308742.9; ENSP00000310120.4; NM_001013620.4; NP_001013642.2.
+DR   UCSC; uc001rln.5; human.
+DR   AGR; HGNC:31088; -.
+DR   CTD; 144245; -.
+DR   DisGeNET; 144245; -.
+DR   GeneCards; ALG10B; -.
+DR   HGNC; HGNC:31088; ALG10B.
+DR   HPA; ENSG00000175548; Low tissue specificity.
+DR   MalaCards; ALG10B; -.
+DR   MIM; 603313; gene.
+DR   OpenTargets; ENSG00000175548; -.
+DR   VEuPathDB; HostDB:ENSG00000175548; -.
+DR   eggNOG; KOG2642; Eukaryota.
+DR   GeneTree; ENSGT00390000012906; -.
+DR   HOGENOM; CLU_017053_1_0_1; -.
+DR   InParanoid; Q5I7T1; -.
+DR   OMA; FPINWNT; -.
+DR   OrthoDB; 4769at2759; -.
+DR   PAN-GO; Q5I7T1; 3 GO annotations based on evolutionary models.
+DR   PhylomeDB; Q5I7T1; -.
+DR   PathwayCommons; Q5I7T1; -.
+DR   Reactome; R-HSA-446193; Biosynthesis of the N-glycan precursor (dolichol lipid-linked oligosaccharide, LLO) and transfer to a nascent protein.
+DR   SignaLink; Q5I7T1; -.
+DR   UniPathway; UPA00378; -.
+DR   BioGRID-ORCS; 144245; 116 hits in 1082 CRISPR screens.
+DR   ChiTaRS; ALG10B; human.
+DR   GenomeRNAi; 144245; -.
+DR   Pharos; Q5I7T1; Tbio.
+DR   PRO; PR:Q5I7T1; -.
+DR   Proteomes; UP000005640; Chromosome 12.
+DR   RNAct; Q5I7T1; protein.
+DR   Bgee; ENSG00000175548; Expressed in calcaneal tendon and 159 other cell types or tissues.
+DR   ExpressionAtlas; Q5I7T1; baseline and differential.
+DR   GO; GO:0005783; C:endoplasmic reticulum; IBA:GO_Central.
+DR   GO; GO:0005789; C:endoplasmic reticulum membrane; IGI:UniProtKB.
+DR   GO; GO:0098553; C:lumenal side of endoplasmic reticulum membrane; IC:UniProt.
+DR   GO; GO:0005886; C:plasma membrane; IDA:UniProtKB.
+DR   GO; GO:0106073; F:dolichyl pyrophosphate Glc2Man9GlcNAc2 alpha-1,2-glucosyltransferase activity; IGI:UniProtKB.
+DR   GO; GO:0006488; P:dolichol-linked oligosaccharide biosynthetic process; IGI:UniProtKB.
+DR   GO; GO:0006487; P:protein N-linked glycosylation; IGI:UniProtKB.
+DR   InterPro; IPR016900; Alg10.
+DR   PANTHER; PTHR12989; ALPHA-1,2-GLUCOSYLTRANSFERASE ALG10; 1.
+DR   PANTHER; PTHR12989:SF12; DOL-P-GLC:GLC(2)MAN(9)GLCNAC(2)-PP-DOL ALPHA-1,2-GLUCOSYLTRANSFERASE-RELATED; 1.
+DR   Pfam; PF04922; DIE2_ALG10; 1.
+DR   PIRSF; PIRSF028810; Alpha1_2_glucosyltferase_Alg10; 1.
+DR   neXtProt; NX_Q5I7T1; -.
+DR   PharmGKB; PA134936082; -.
+DR   TreeFam; TF300150; -.
+PE   1: Evidence at protein level;
+KW   Endoplasmic reticulum; Glycosyltransferase; Membrane; Reference proteome;
+KW   Transferase; Transmembrane; Transmembrane helix.
+FT   CHAIN           1..473
+FT                   /note="Dol-P-Glc:Glc(2)Man(9)GlcNAc(2)-PP-Dol alpha-1,2-
+FT                   glucosyltransferase"
+FT                   /id="PRO_0000215448"
+FT   TOPO_DOM        1..6
+FT                   /note="Cytoplasmic"
+FT                   /evidence="ECO:0000255"
+FT   TRANSMEM        7..27
+FT                   /note="Helical"
+FT                   /evidence="ECO:0000255"
+FT   TOPO_DOM        28..64
+FT                   /note="Extracellular"
+FT                   /evidence="ECO:0000255"
+FT   TRANSMEM        65..85
+FT                   /note="Helical"
+FT                   /evidence="ECO:0000255"
+FT   TOPO_DOM        86..97
+FT                   /note="Cytoplasmic"
+FT                   /evidence="ECO:0000255"
+FT   TRANSMEM        98..118
+FT                   /note="Helical"
+FT                   /evidence="ECO:0000255"
+FT   TOPO_DOM        119..126
+FT                   /note="Extracellular"
+FT                   /evidence="ECO:0000255"
+FT   TRANSMEM        127..147
+FT                   /note="Helical"
+FT                   /evidence="ECO:0000255"
+FT   TOPO_DOM        148..150
+FT                   /note="Cytoplasmic"
+FT                   /evidence="ECO:0000255"
+FT   TRANSMEM        151..171
+FT                   /note="Helical"
+FT                   /evidence="ECO:0000255"
+FT   TOPO_DOM        172..175
+FT                   /note="Extracellular"
+FT                   /evidence="ECO:0000255"
+FT   TRANSMEM        176..196
+FT                   /note="Helical"
+FT                   /evidence="ECO:0000255"
+FT   TOPO_DOM        197..256
+FT                   /note="Cytoplasmic"
+FT                   /evidence="ECO:0000255"
+FT   TRANSMEM        257..277
+FT                   /note="Helical"
+FT                   /evidence="ECO:0000255"
+FT   TOPO_DOM        278..283
+FT                   /note="Extracellular"
+FT                   /evidence="ECO:0000255"
+FT   TRANSMEM        284..304
+FT                   /note="Helical"
+FT                   /evidence="ECO:0000255"
+FT   TOPO_DOM        305..317
+FT                   /note="Cytoplasmic"
+FT                   /evidence="ECO:0000255"
+FT   TRANSMEM        318..338
+FT                   /note="Helical"
+FT                   /evidence="ECO:0000255"
+FT   TOPO_DOM        339..365
+FT                   /note="Extracellular"
+FT                   /evidence="ECO:0000255"
+FT   TRANSMEM        366..386
+FT                   /note="Helical"
+FT                   /evidence="ECO:0000255"
+FT   TOPO_DOM        387..392
+FT                   /note="Cytoplasmic"
+FT                   /evidence="ECO:0000255"
+FT   TRANSMEM        393..413
+FT                   /note="Helical"
+FT                   /evidence="ECO:0000255"
+FT   TOPO_DOM        414..436
+FT                   /note="Extracellular"
+FT                   /evidence="ECO:0000255"
+FT   TRANSMEM        437..457
+FT                   /note="Helical"
+FT                   /evidence="ECO:0000255"
+FT   TOPO_DOM        458..473
+FT                   /note="Cytoplasmic"
+FT                   /evidence="ECO:0000255"
+FT   VARIANT         33
+FT                   /note="E -> D (found in a patient with ventricular
+FT                   fibrillation and QT prolongation; uncertain significance;
+FT                   dbSNP:rs188043534)"
+FT                   /evidence="ECO:0000269|PubMed:20950623"
+FT                   /id="VAR_089086"
+FT   VARIANT         84
+FT                   /note="A -> G (in dbSNP:rs6582584)"
+FT                   /evidence="ECO:0000269|PubMed:14525949,
+FT                   ECO:0000269|PubMed:15489334, ECO:0000269|Ref.4"
+FT                   /id="VAR_048217"
+FT   VARIANT         383
+FT                   /note="S -> N (in dbSNP:rs57963306)"
+FT                   /id="VAR_061002"
+FT   VARIANT         446
+FT                   /note="I -> V (in dbSNP:rs61730283)"
+FT                   /evidence="ECO:0000269|PubMed:15280551"
+FT                   /id="VAR_023753"
+SQ   SEQUENCE   473 AA;  55448 MW;  47D0CF7B6C84B8EC CRC64;
+     MAQLEGYCFS AALSCTFLVS CLLFSAFSRA LREPYMDEIF HLPQAQRYCE GHFSLSQWDP
+     MITTLPGLYL VSVGVVKPAI WIFAWSEHVV CSIGMLRFVN LLFSVGNFYL LYLLFHKVQP
+     RNKAASSIQR VLSTLTLAVF PTLYFFNFLY YTEAGSMFFT LFAYLMCLYG NHKTSAFLGF
+     CGFMFRQTNI IWAVFCAGNV IAQKLTEAWK TELQKKEDRL PPIKGPFAEF RKILQFLLAY
+     SMSFKNLSML FCLTWPYILL GFLFCAFVVV NGGIVIGDRS SHEACLHFPQ LFYFFSFTLF
+     FSFPHLLSPS KIKTFLSLVW KHGILFLVVT LVSVFLVWKF TYAHKYLLAD NRHYTFYVWK
+     RVFQRYAILK YLLVPAYIFA GWSIADSLKS KPIFWNLMFF ICLFIVIVPQ KLLEFRYFIL
+     PYVIYRLNIT LPPTSRLVCE LSCYAIVNFI TFYIFLNKTF QWPNSQDIQR FMW
+//
+https://api.ncbi.nlm.nih.gov/datasets/v2/gene/id/144245/download?include_annotation_type=FASTA_RNA&include_annotation_type=FASTA_PROTEIN&aux_report=PRODUCT_REPORT
